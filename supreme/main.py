4dbfb084749b246842cf02cb64e009ef551b8c89
phongtran715 <phongtran0715@gmail.com>
2021-01-07 17:18:28 +0700

    Update UI for supreme

--- supreme/main.py
+++ supreme/main.py
@@ -1,5 +1,6 @@
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import *
+from profile_manager import ProfileWindown
 import sys
 import os
 
@@ -8,44 +9,11 @@ class MainWindow(QtWidgets.QMainWindow):
 	def __init__(self):
 		super(MainWindow, self).__init__()
 		uic.loadUi(os.path.join("ui", "supreme.ui"), self)
+		self.actionProfile_Manager.triggered.connect(self.on_actionProfileManaget_triggered)
 
-		self.profileManager = ProfileWindow()
-		
-		# set table header
-		# self.tbListTask.setHorizontalHeaderLabels(['ID', 'ITEM', 'CATEGORY', 'SIZE', 'COLOUR', 'BILLING PROFILE', 'STATUS'])
-
-		# set button actions
-		self.btnStartAll.clicked.connect(self.startAllButtonPress)
-		self.btnCreateTask.clicked.connect(self.btnCreateTaskPress)
-		self.actionProfile_Manager.triggered.connect(self.actionProfileManagetPress)
-		self.show()
-
-	def startAllButtonPress(self):
-		print("start All button press");
-
-	def btnCreateTaskPress(self):
-		print("create new task")
-
-	def actionProfileManagetPress(self):
-		print("Profile manager press")
-		self.profileManager.show()
-
-
-class ProfileWindow(QtWidgets.QMainWindow):
-	def __init__(self, parent=None):
-		super(ProfileWindow, self).__init__(parent)
-		uic.loadUi(os.path.join("ui", "profile.ui"))
-
-
-class EditProfileWindow(QtWidgets.QMainWindow):
-	def __init__(self):
-		super(EditProfileWindow, self).__init__()
-		uic.loadUi(os.path.join("ui", "edit_profile.ui"))
-
-class AddNewProfileWindow(QtWidgets.QMainWindow):
-	def __init__(self):
-		super(AddNewProfileWindow, self).__init__()
-		uic.loadUi(os.path.join("ui", "add_new_profile.ui"))
+	def on_actionProfileManaget_triggered(self):
+		self.profileFrm = ProfileWindown()
+		self.profileFrm.show()
 
 
 if __name__ == "__main__":
