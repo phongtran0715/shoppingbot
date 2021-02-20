35a3ffb8ca49fd117c783b1f36bcea9db683350d
phongtran715 <phongtran0715@gmail.com>
2021-02-20 10:52:35 +0700

    Implement account manager function

--- bird_bot/pages/accounts_setting_page.py
+++ bird_bot/pages/accounts_setting_page.py
@@ -2,6 +2,9 @@ import os
 from PyQt5 import QtWidgets, uic, QtGui, QtCore
 from PyQt5.QtWidgets import *
 from pages.new_account_page import NewAccount
+from model.account_model import AccountModel
+from utils import return_data,write_data,get_profile,Encryption
+
 
 class AccountsManager(QtWidgets.QMainWindow):
 	def __init__(self):
@@ -15,33 +18,54 @@ class AccountsManager(QtWidgets.QMainWindow):
 		self.btnEdit.clicked.connect(self.btnEdit_clicked)
 		self.btnDelete.clicked.connect(self.btnDelete_clicked)
 
+		self.load_account_data()
+
 	def center(self):
 		qr = self.frameGeometry()
 		cp = QDesktopWidget().availableGeometry().center()
 		qr.moveCenter(cp)
 		self.move(qr.topLeft())
 
+	def load_account_data(self):
+		self.tbAccount.setRowCount(0)
+		accounts = return_data("./data/accounts.json")
+		for account in accounts:
+			row_count = self.tbAccount.rowCount()
+			self.tbAccount.setRowCount(row_count + 1)
+			self.tbAccount.setItem(row_count, 0, QTableWidgetItem(account['account_type']))
+			self.tbAccount.setItem(row_count, 1, QTableWidgetItem(account['name']))
+			self.tbAccount.setItem(row_count, 2, QTableWidgetItem(account['user_name']))
+			self.tbAccount.setItem(row_count, 3, QTableWidgetItem(account['password']))
+			self.tbAccount.setItem(row_count, 4, QTableWidgetItem(account['proxy']))
+			self.tbAccount.setItem(row_count, 5, QTableWidgetItem(account['profile']))
+
 	def btnAdd_clicked(self):
 		self.createAccountFrm = NewAccount()
 		if self.createAccountFrm.exec_() == QtWidgets.QDialog.Accepted:
-			pass
+			self.createAccountFrm.save_account()
+			self.load_account_data()
 
 	def btnEdit_clicked(self):
 		index = self.tbAccount.currentRow()
 		if index >= 0:
-			account_name = self.tbAccount.item(index, 0).text()
+			account_name = self.tbAccount.item(index, 1).text()
 			self.editAccountFrm = NewAccount('modify', account_name)
-			self.editAccountFrm.loadEditData()
+			self.editAccountFrm.load_edit_data()
 			if self.editAccountFrm.exec_() == QtWidgets.QDialog.Accepted:
-				pass
+				self.editAccountFrm.update_account()
+				self.load_account_data()
 		else:
 			QMessageBox.critical(self, "Birdbot", 'You must select one account!',)
 
 	def btnDelete_clicked(self):
 		index = self.tbAccount.currentRow()
 		if index >= 0:
-			account_name = self.tbAccount.item(index, 0).text()
-			pass
-
+			account_name = self.tbAccount.item(index, 1).text()
+			accounts = return_data("./data/accounts.json")
+			for account in accounts:
+				if account['name'] == account_name:
+					accounts.remove(account)
+			write_data("./data/accounts.json",accounts)
+			self.load_account_data()
 		else:
 			QMessageBox.critical(self, "Birdbot", 'You must select one account!',)
--- bird_bot/pages/new_account_page.py
+++ bird_bot/pages/new_account_page.py
@@ -1,6 +1,8 @@
 import os
 from PyQt5 import QtWidgets, uic, QtGui, QtCore
 from PyQt5.QtWidgets import *
+from utils import return_data,write_data,get_profile,Encryption
+
 
 class NewAccount(QtWidgets.QDialog):
 	def __init__(self, mode='new', account_name=None):
@@ -10,6 +12,7 @@ class NewAccount(QtWidgets.QDialog):
 		self.center()
 		self.mode = mode
 		self.account_name = account_name
+		self.init_data()
 
 	def center(self):
 		qr = self.frameGeometry()
@@ -17,6 +20,61 @@ class NewAccount(QtWidgets.QDialog):
 		qr.moveCenter(cp)
 		self.move(qr.topLeft())
 
-	def loadEditData(self):
-		# TODO: load data from json
-		pass
+	def init_data(self):
+		proxies = return_data("./data/proxies.json")
+		for proxy in proxies:
+			self.cbProxy.addItem(proxy['list_name'])
+
+		profiles = return_data("./data/profiles.json")
+		for profile in profiles:
+			self.cbProfile.addItem(profile['profile_name'])
+
+	def load_edit_data(self):
+		accounts = return_data("./data/accounts.json")
+		for account in accounts:
+			if account['name'] == self.account_name:
+				index = self.cbType.findText(account['account_type'], QtCore.Qt.MatchFixedString)
+				if index >= 0:
+					self.cbType.setCurrentIndex(index)
+
+				self.txtName.setText(account['name'])
+				self.txtUsername.setText(account['user_name'])
+				self.txtPassword.setText(account['password'])
+
+				index = self.cbProxy.findText(account['proxy'], QtCore.Qt.MatchFixedString)
+				if index >= 0:
+					self.cbProxy.setCurrentIndex(index)
+
+				index = self.cbProfile.findText(account['profile'], QtCore.Qt.MatchFixedString)
+				if index >= 0:
+					self.cbProfile.setCurrentIndex(index)
+
+	def update_account(self):
+		account_data = {
+			'account_type' : self.cbType.currentText(),
+			'name' : self.txtName.text(),
+			'user_name' : self.txtUsername.text(),
+			'password' : self.txtPassword.text(),
+			'proxy' : self.cbProxy.currentText(),
+			'profile' : self.cbProfile.currentText()
+		}
+		accounts = return_data("./data/accounts.json")
+		for account in accounts:
+			if account['name'] == self.txtName.text():
+				accounts.remove(account)
+
+		accounts.append(account_data)
+		write_data("./data/accounts.json",accounts)
+
+	def save_account(self):
+		account_data = {
+			'account_type' : self.cbType.currentText(),
+			'name' : self.txtName.text(),
+			'user_name' : self.txtUsername.text(),
+			'password' : self.txtPassword.text(),
+			'proxy' : self.cbProxy.currentText(),
+			'profile' : self.cbProfile.currentText()
+		}
+		accounts = return_data("./data/accounts.json")
+		accounts.append(account_data)
+		write_data("./data/accounts.json",accounts)
